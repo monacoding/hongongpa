0159db53bb908e0019de4c7bb735ce2ac7c442c4
김태형 <symphony5809@gmail.com>
2025-03-15 11:33:23 +0900

    5강

---
+++ 강의/CH_05.py
@@ -0,0 +1,28 @@
+#주석 
+# # 뒤에 있는 모든 내용은 전부다 주석 처리 됨, 즉 무시 됨 
+print("주석은 이렇게 시작 됩니다.")# 이렇게도 주석을 달 수 있음 
+print(10+10)
+
+#출력 print() 스네이크 케이스, 함수
+#print함수는 일반적으로 3가지로 쓰일 수 잇다. 
+
+#하나 출력하기 
+print(10)
+#여러개 출력하기
+print(10,2030405,50)
+#줄바꿈 하기
+print()
+
+#하나만 출력 합니다. 
+print("#하나만 출력 합니다.")
+print("hello pytyon programming ")
+print()
+
+#여러개를 출력합니다.
+print("#여러개를 출력합니다.")
+print("hello","python","programming")
+
+#아무것도 입력하지 않으면 단순하게 줄바꿈이 됩니다. 
+print() 
+
+print("Hello Python")
